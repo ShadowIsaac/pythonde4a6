3439eec6121163df55dc39ac07db46f46af16ee1
ShadowIsaac <159494855+ShadowIsaac@users.noreply.github.com>
2024-02-27 17:44:02 -0600

    Add files via upload

---
+++ Parameros.py
@@ -0,0 +1,15 @@
+
+Numero1 = 9
+Numero2 = 2
+Numero3 = 4
+
+def multiplicale(Numero1,Numero2,Numero3):
+    total=Numero1*Numero2*Numero3
+    print (total)
+    print ("Ahora multiplicando otra funcion")
+    return total
+
+totalfuncion = multiplicale (Numero1,Numero2,Numero3)
+print (totalfuncion)
+
+print (totalfuncion)
